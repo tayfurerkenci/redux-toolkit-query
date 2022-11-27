ad5e317b11543a8ef0b00dcc19fe7fcff3c055a3
Tayfur Erkenci <terkenci@gmail.com>
2022-11-28 00:51:50 +0300

    feat: implemented fetch users thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store';
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <div>UsersList</div>;
 };
 
