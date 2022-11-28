23141b3566370562a8f3bbe09e5266be77b3ff15
Tayfur Erkenci <terkenci@gmail.com>
2022-11-29 00:22:41 +0300

    feat: implemented use-thunk custom hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,58 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchUsers } from '../store';
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from '../elements/Button';
+import { Skeleton } from '../elements/Skeleton';
+import { useThunk } from '../hooks/use-thunk';
 
 const UsersList = () => {
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
-  return <div>UsersList</div>;
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  if (isLoadingUsers) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+
+  if (loadingUsersError) {
+    return <div>Error fetcing data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          'Creating User...'
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && 'Error creating user...'}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
